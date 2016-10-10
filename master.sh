230c589487dd9873f44f2b71aa9d5587533b2b5d
Abe Handler <akh2103@gmail.com>
2016-10-10 14:15:52 -0400

    mores

--- master.sh
+++ master.sh
@@ -1,17 +1,18 @@
 ## Prelim stuff
 
-# rm demos/*anno
-# python makedemos.py
+python makedemos.py
 
 ## Real stuff
 
 #THIS DOES NOT WORK W/O LC_ALL=C 
 #http://man7.org/linux/man-pages/man1/sort.1.html
 
-# LC_ALL=C sort -k2 -n -t"," shingles.congress > shingles.congress.sorted
+# RUN STUFF MANUALLY IN THE SHINGLING FILE HERE FOR NOW BAD TODO
 
-# rm "shingles.congress.candidates"
-# python analyzer2.py  # make the candidates
+LC_ALL=C sort -k2 -n -t"," shingles.congress > shingles.congress.sorted
+
+rm "shingles.congress.candidates"
+python analyzer2.py  # make the candidates
 
 rm "shingles.congress.candidates.jacs"
 FILENAME="shingles.congress.candidates"
