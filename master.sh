2d01e6c6d11eca4cb1bcd4716df45af2cd5d0d5f
Abe Handler <akh2103@gmail.com>
2016-10-10 20:34:41 -0400

    mores

--- master.sh
+++ master.sh
@@ -1,13 +1,21 @@
 ## Prelim stuff
-
-python makedemos.py
+# python makedemos.py
 
 ## Real stuff
 
 #THIS DOES NOT WORK W/O LC_ALL=C 
 #http://man7.org/linux/man-pages/man1/sort.1.html
 
-# RUN STUFF MANUALLY IN THE SHINGLING FILE HERE FOR NOW BAD TODO
+echo "fn,digit,window_no,window_size,iter" > "shingles.congress"
+
+FILES="demos_congress/*anno"
+for (( c=1; c<=100; c++ ))
+do
+    for f in $FILES
+    do
+        python shingling.py -doc "$f" -iter "$c"
+    done
+done
 
 LC_ALL=C sort -k2 -n -t"," shingles.congress > shingles.congress.sorted
 
@@ -18,5 +26,5 @@ rm "shingles.congress.candidates.jacs"
 FILENAME="shingles.congress.candidates"
 cat $FILENAME | while read LINE
 do
-python check_candidate.py "$LINE"
+   python check_candidate.py "$LINE"
 done
