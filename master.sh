e22d48785d53184cfb78b87d0974fa3b05d025c0
Abram Handler <ahandler@hobbes.cs.umass.edu>
2016-10-17 14:37:58 -0400

    before big run

--- master.sh
+++ master.sh
@@ -12,7 +12,7 @@ echo "[*] shingling"
 
 #FILES="/data/bills/mdenny_copy_early2015/bills/*anno"
 #FILES="/data/bills/mdenny_copy_early2015/bills/POS_Tagged_Bills/*anno"
-for (( c=1; c<=10; c++ ))
+for (( c=0; c<100; c++ ))
 do
     python pi.py
     find /data/bills/mdenny_copy_early2015/bills/POS_Tagged_Bills/ -type f -name '*.anno' | parallel --eta -j20 python shingling.py -doc {} -iter "$c"
@@ -20,15 +20,16 @@ done
 
 echo "[*] sorting"
 
-# LC_ALL=C sort -k2 -n -t"," shingles.congress > shingles.congress.sorted
+LC_ALL=C sort -k2 -n -t"," shingles.congress > shingles.congress.sorted
 
 
-#echo "[*] finding candidates"
-#rm "shingles.congress.candidates"
-#python analyzer2.py  # make the candidates
+echo "[*] finding candidates"
+rm "shingles.congress.candidates"
+python analyzer2.py  # make the candidates
 
-#echo "[*] finding jaccards"
+echo "[*] finding jaccards"
 rm shingles.congress.candidates.jacs
+
 
 sort shingles.congress.candidates | uniq > t
 mv t shingles.congress.candidates
