4f8cecfac02e4a9d3c94c2b6bf65ec52a9d71868
Abe Handler <akh2103@gmail.com>
2016-10-24 20:47:56 -0400

    more

--- all_pairs.py
+++ all_pairs.py
@@ -11,29 +11,10 @@ Candidate = collections.namedtuple('Candidate', 'fn1 windowno1 fn2 windowno2')
 
 def process_digit(current_list):
     '''find reasonable comparisons based on information from this digit'''
-    candidates = []
-    if len(current_list) == 1:
-        return
-
-    # all files for just one digit
-    assert len(set([a.split(",")[1] for a in current_list])) < 2
-    # find resonable candidates
-    for log_item in current_list:
-        fn, digit, window, window_size, iter_ = log_item.split(",")
-        candidates.append(D_i(fn=fn, windowno=window))
-    
-    reasonable_candidates = []
-    for pair in itertools.product(candidates, candidates):
+    for pair in itertools.product(current_list, current_list):
         a, b = pair
-        if a.fn != b.fn:
-            c = Candidate(fn1=a.fn, fn2=b.fn, windowno1=a.windowno, windowno2=b.windowno)
-            reasonable_candidates.append(c)
-    for candidate in reasonable_candidates:
         with open("shingles.congress.candidates", "a") as outf:
-            outf.write("{},{},{},{}\n".format(candidate.fn1,
-                                              candidate.fn2,
-                                              candidate.windowno1,
-                                              candidate.windowno2))
+            outf.write("{},{}\n".format(a, b))
 
 import re
 digitgetter = re.compile("(?<=,)[0-9]+")
