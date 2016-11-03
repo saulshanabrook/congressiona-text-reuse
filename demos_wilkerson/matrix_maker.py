62767858c00c6fb32cc5b87ed503f3b7af5be4d2
Abe Handler <akh2103@gmail.com>
2016-11-02 23:40:49 -0500

    stuff

--- demos_wilkerson/matrix_maker.py
+++ demos_wilkerson/matrix_maker.py
@@ -16,13 +16,26 @@ def find_ngrams(input_list, n):
 def vocab(input_dict):
     '''numberize'''
     fc = [v["5-gms"] for k,v in input_dict.items()]
-    vocab_ = set(reduce(lambda x,y: x+y, fc))
+    vocab_ = set(reduce(lambda x,y: x.union(y), fc))
     return {v:k for k,v in enumerate(vocab_)}
+
+
+def get_ordered_dates():
+    files_dates = []
+    for fno, f in enumerate(files):
+        mo = re.search("[a-z]+_[0-9]+_[0-9]+.txt", f).group()
+        month = int(mo_mapper[mo.split("_")[0]])
+        day = int(mo.split("_")[1])
+        year = int(mo.split("_")[2].replace(".txt", ""))
+        dt = datetime(year, month, day)
+        files_dates.append((dt, f))
+    files_dates.sort(key=lambda x:x[0])
+    return [p[1] for p in files_dates]
 
 
 def build_corpus():
     data = {}
-    for fno, f in enumerate(files):
+    for fno, f in enumerate(get_ordered_dates()):
         with open(f, "r") as inf:
             txt = inf.read().split()
             mo = re.search("[a-z]+_[0-9]+_[0-9]+.txt", f).group()
@@ -31,7 +44,7 @@ def build_corpus():
             year = int(mo.split("_")[2].replace(".txt", ""))
             dt = datetime(year, month, day)
             ngrams = find_ngrams(txt, 5)
-            data[fno] = {"date": dt.strftime("%Y-%m-%d"), "5-gms": ngrams}
+            data[fno] = {"date": dt.strftime("%Y-%m-%d"), "5-gms": set(ngrams)}
     return data
 
 data = build_corpus()
@@ -45,3 +58,16 @@ for doc, items in data.items():
         n = V[fgm]
         mx[n][doc] = 1
 
+def name_note(time, txt):
+    '''naming convention for nodes'''
+    return "{}-{}".format(time, V[txt])
+
+# represents a sparse matrix. Each tuple (i,j) means 5-gm i is parent of j
+# you can imagine this as a big matrix: ndocs * |V| rows and cols
+parents = []
+for dno in range(2, len(data)):
+    for fvgrm in data[dno]['5-gms']:
+        if fvgrm in data[dno - 1]['5-gms']:
+            parents.append((name_note(dno - 1, fvgrm), name_note(dno, fvgrm)))
+
+print len(parents), len(V)
