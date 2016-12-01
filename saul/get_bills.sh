abbf4c68264b5a90241af13af034392e0554fa77
Abe Handler <akh2103@gmail.com>
2016-12-01 17:10:38 -0500

    fix

--- saul/get_bills.sh
+++ saul/get_bills.sh
@@ -2,6 +2,8 @@
 COUNTER=101
 while [  $COUNTER -lt 114 ]; do
     echo The counter is $COUNTER
-    rsync -avz govtrack.us::govtrackdata/congress/$COUNTER/bills govtrackdata
+    mkdir govtrackdata/$COUNTER
+    mkdir govtrackdata/$COUNTER/sections
+    rsync -avz govtrack.us::govtrackdata/congress/$COUNTER/bills govtrackdata/$COUNTER/
     let COUNTER=COUNTER+1
 done
