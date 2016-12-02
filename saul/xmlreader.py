dc24277fd00754598493215541863d27fa8c589d
Abe Handler <akh2103@gmail.com>
2016-12-01 21:17:58 -0500

    multicongress

--- saul/xmlreader.py
+++ saul/xmlreader.py
@@ -1,8 +1,12 @@
 import xml.etree.ElementTree
 
+import sys
+
 import glob
 
-for fno, fn in enumerate(open("all.txt", "r")):
+congress = sys.argv[1]
+
+for fno, fn in enumerate(open(congress + ".all", "r")):
     fn = fn.replace("\n", "")
     if fno % 1000 == 0:
         print fno
@@ -13,7 +17,7 @@ for fno, fn in enumerate(open("all.txt", "r")):
             section_text = "".join(section.itertext())
             id = section.attrib["id"]
             bill_no = fn.split('/').pop()
-            with open("sections/" + id + "_" + fn.replace(".xml", ".txt").replace("/", "_"), "w") as outf:
+            with open("govtrackdata/" + congress + "/sections/" + id + "_" + fn.replace(".xml", ".txt").replace("/", "_"), "w") as outf:
                 outf.write(section_text.encode("ascii", "ignore"))
     except xml.etree.ElementTree.ParseError:
         print "ERROR"
